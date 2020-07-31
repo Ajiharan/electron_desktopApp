c55980d7edae5683d0cdbc70010e304f7098933a
Ajiharan <53114753+Ajiharan@users.noreply.github.com>
2020-07-31 11:34:01 +0530

    year&semister added

--- electron_desktop/src/screens/year_semister/Yearsemister.js
+++ electron_desktop/src/screens/year_semister/Yearsemister.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './Yearsemister.css';
+import {db} from '../../firebase';
+import firebase from 'firebase';
+
 const Yearsemister = () => {
+
+    const [year,setYear]=useState("");
+
+    const submitHandler=(e)=>{
+        e.preventDefault();
+        db.collection('students').add({
+            year_semister:year,
+            timestamp:firebase.firestore.FieldValue.serverTimestamp()
+        });
+        setYear("");
+    }
+
+    const clearInput=()=>{
+        setYear("");
+    }
+ 
+
+
     return (
-        <div className="container yearSemister">
-            <h2>Add year & Semister</h2>
-            <div className="form-group">
-                <label>AA</label>
+        <div className="yearSemister__container">
+            <div className="yearSemister">
+                <h2>Add year & Semister</h2>
+                <form id="frm" onSubmit={e=>submitHandler(e)}>
+                    <div className="yearSemister_inputs">
+                        <label htmlFor="year_semister">year & semister</label>
+                        <input value={year} onChange={(e)=>setYear(e.target.value)}
+                        placeholder="eg:S1.Y3" id="year_semister" type="text" className="form-control" required/>
+                    </div>
+                    <div className="yearSemister_buttons">
+                        <button type="submit" class="btn" disabled={!year}>Add</button>
+                        <button type="button" class="btn">View</button>
+                        <button type="button" onClick={clearInput} class="btn">Clear</button>
+                    </div>
+                
+                </form>
             </div>
         </div>
+      
     );
 };
 
