8531d30bd8956a7b6c9fdfc40ed89ddf47db9fab
Ajiharan <53114753+Ajiharan@users.noreply.github.com>
2020-08-17 22:41:44 +0530

    fixed bugs

--- electron_desktop/src/redux/Room/RoomAction.js
+++ electron_desktop/src/redux/Room/RoomAction.js
@@ -1,131 +1,110 @@
 import {
-    ADD_ROOM_FAILURE,
-    ADD_ROOM_REQUEST,
-    ADD_ROOM_SUCCESS,
-    GET_ONE_ROOM_FAILURE,
-    GET_ONE_ROOM_REQUEST,
-    GET_ONE_ROOM_SUCCESS,
-    GET_ROOM_FAILURE,
-    GET_ROOM_REQUEST,
-    GET_ROOM_SUCCESS,
-    UPDATE_ROOM_FAILURE,
-    UPDATE_ROOM_REQUEST,
-    UPDATE_ROOM_SUCCESS,
-  } from "./RoomType";
-  
+  ADD_ROOM_FAILURE,
+  ADD_ROOM_REQUEST,
+  ADD_ROOM_SUCCESS,
+  GET_ONE_ROOM_FAILURE,
+  GET_ONE_ROOM_REQUEST,
+  GET_ONE_ROOM_SUCCESS,
+  GET_ROOM_FAILURE,
+  GET_ROOM_REQUEST,
+  GET_ROOM_SUCCESS,
+  UPDATE_ROOM_FAILURE,
+  UPDATE_ROOM_REQUEST,
+  UPDATE_ROOM_SUCCESS,
+} from "./RoomType";
+
 import { db } from "../../firebase";
 import firebase from "firebase";
 
-const addRoom = (room) => {
-    return async (dispatch) => {
-      dispatch({ type: ADD_ROOM_REQUEST });
-      try {
-        const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-        db.collection("rooms")
-          .get()
-          .then(async (snapshot) => {
-            const tempData = await snapshot.docs.map((doc) => ({
-              room: doc.data().room,
-              id: doc.id,
-            }));
-  
-            const isExists = await tempData.filter(
-              (data) => data.room === room
-            );
-            // console.log("isExists", isExists);
-            if (isExists.length === 0) {
-              db.collection("rooms")
-                .add({
-                  room,
-                  timestamp,
-                })
-                .then(() => {
-                  dispatch({
-                    type: ADD_ROOM_SUCCESS,
-                    payload: {
-                      room,
-                      timestamp,
-                    },
-                  });
-                })
-                .catch((err) => {
-                  dispatch({
-                    type: ADD_ROOM_FAILURE,
-                    error: err,
-                  });
-                });
-            } else {
-              dispatch({
-                type: ADD_ROOM_FAILURE,
-                error: "data already exists",
-              });
-            }
-          });
-      } catch (err) {}
-    };
-  };
-  const viewRoom = () => {
-    return async (dispatch) => {
-      dispatch({ type: GET_ROOM_REQUEST });
-      try {
-        db.collection("rooms")
-          .orderBy("timestamp", "desc")
-          .onSnapshot(async (snapshot) => {
-            const tempData = await snapshot.docs.map((doc) => ({
-              room: doc.data().room,
-              id: doc.id,
-              timestamp: doc.data().timestamp,
-            }));
-            console.log("getTemp Data", tempData);
-            dispatch({
-              type: GET_ROOM_SUCCESS,
-              payload: tempData,
-            });
-          })
-          .catch((err) => {
-            dispatch({
-              type: GET_ROOM_FAILURE,
-              error: err,
-            });
-          });
-      } catch (err) {}
-    };
-  };
-  
-  const UpdateRoom = (doc_id, input) => {
-    return async (dispatch) => {
-      dispatch({ type: UPDATE_ROOM_REQUEST });
-      try {
-        const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-        db.collection("rooms")
-          .doc(doc_id)
-          .set(
-            {
-              room: input,
+const addRoom = (roomData) => {
+  return async (dispatch) => {
+    dispatch({ type: ADD_ROOM_REQUEST });
+    try {
+      const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+      db.collection("rooms")
+        .add({
+          ...roomData,
+          timestamp,
+        })
+        .then(() => {
+          dispatch({
+            type: ADD_ROOM_SUCCESS,
+            payload: {
+              ...roomData,
               timestamp,
             },
-            { merge: true }
-          )
-          .then(() => {
-            dispatch({
-              type: UPDATE_ROOM_SUCCESS,
-              message: "update Sucessfully",
-            });
-          })
-          .catch((err) => {
-            dispatch({
-              type: UPDATE_ROOM_FAILURE,
-              error: err,
-            });
           });
-      } catch (err) {
-        dispatch({
-          type: UPDATE_ROOM_FAILURE,
-          error: err,
+        })
+        .catch((err) => {
+          dispatch({
+            type: ADD_ROOM_FAILURE,
+            error: err,
+          });
         });
-      }
-    };
+    } catch (err) {}
   };
-  
-  export { addRoom, viewRoom, UpdateRoom };
-  
+};
+const viewRoom = () => {
+  return async (dispatch) => {
+    dispatch({ type: GET_ROOM_REQUEST });
+    try {
+      db.collection("rooms")
+        .orderBy("timestamp", "desc")
+        .onSnapshot(async (snapshot) => {
+          const tempData = await snapshot.docs.map((doc) => ({
+            room: doc.data(),
+            id: doc.id,
+            timestamp: doc.data().timestamp,
+          }));
+          console.log("getTemp Data", tempData);
+          dispatch({
+            type: GET_ROOM_SUCCESS,
+            payload: tempData,
+          });
+        })
+        .catch((err) => {
+          dispatch({
+            type: GET_ROOM_FAILURE,
+            error: err,
+          });
+        });
+    } catch (err) {}
+  };
+};
+
+const UpdateRoom = (doc_id, input) => {
+  return async (dispatch) => {
+    dispatch({ type: UPDATE_ROOM_REQUEST });
+    try {
+      const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+      db.collection("rooms")
+        .doc(doc_id)
+        .set(
+          {
+            room: input,
+            timestamp,
+          },
+          { merge: true }
+        )
+        .then(() => {
+          dispatch({
+            type: UPDATE_ROOM_SUCCESS,
+            message: "update Sucessfully",
+          });
+        })
+        .catch((err) => {
+          dispatch({
+            type: UPDATE_ROOM_FAILURE,
+            error: err,
+          });
+        });
+    } catch (err) {
+      dispatch({
+        type: UPDATE_ROOM_FAILURE,
+        error: err,
+      });
+    }
+  };
+};
+
+export { addRoom, viewRoom, UpdateRoom };
--- electron_desktop/src/screens/Room/Room.js
+++ electron_desktop/src/screens/Room/Room.js
@@ -1,127 +1,133 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./Room.css";
-import {Spinner} from "../animations/Spinner";
-import {DotLoader} from "react-spinners";
-import {addRoom} from "../../redux/Room/RoomAction";
+import { Spinner } from "../animations/Spinner";
+import { DotLoader } from "react-spinners";
+import { addRoom } from "../../redux/Room/RoomAction";
 import ScreenNav from "../screen-nav/ScreenNav";
-import {useFormik} from 'formik'
+import { useFormik } from "formik";
 
 const Room = () => {
-    
-    const dispatch = useDispatch();
-    const [clicked, isClicked] = useState(false);
-    const [success, setSuccess] = useState("Successfully Added!");
-    const { loading, error, room } = useSelector(
-        (state) => state.room_add
-    );
+  const dispatch = useDispatch();
+  const [clicked, isClicked] = useState(false);
+  const [success, setSuccess] = useState("Successfully Added!");
+  const { loading, error, room } = useSelector((state) => state.room_add);
 
-    const formik = useFormik({
-        initialValues : {
-            name : '',
-            type :'',
-            building:'',
-            capacity:0, 
-        },
-        onSubmit :(inputs) =>{
-            console.log(inputs)
-            isClicked(true);
-            dispatch(addRoom(formik.values.title+"."+formik.values.name,formik.values.type,formik.values.building,formik.values.capacity));
-        }
-    })
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      type: "",
+      building: "",
+      capacity: 0,
+    },
+    onSubmit: (inputs) => {
+      console.log(inputs);
+      isClicked(true);
+      dispatch(addRoom(inputs));
+    },
+  });
 
-    const navData = [
-        {
-            id: 1,
-            name: "TimeTable > ",
-            pathname: "/",
-          },
-          {
-            id: 2,
-            name: "Room",
-            pathname: "/room/room/add",
-          },
-    ];
+  const navData = [
+    {
+      id: 1,
+      name: "TimeTable > ",
+      pathname: "/",
+    },
+    {
+      id: 2,
+      name: "Room",
+      pathname: "/room/room/add",
+    },
+  ];
 
-        return (
-
-            <div className="room">
-                <ScreenNav rightNavData={navData} />
-                <div className="room__container">
-                    <div className="room__box">
-                        <div className="lead text-success room__message">
-                            {loading && clicked && <Spinner Loader={DotLoader} size={30} />}
-                            <p className={`lead ${error ? "text-danger" : "text-success"}`}>
-                                {!loading && !error && success}
-                                {!loading && error && error}
-                            </p>
-                        </div>
-                        <h2 className="text-center text-dark">Add Room Details</h2>
-                        <form id="myForm" onSubmit={formik.handleSubmit} autoComplete="off">
-
-
-                            <div className="room_inputs">
-                                <label htmlFor="name">Room Name</label>
-                                <input
-                                    placeholder="A640"
-                                    name="name"
-                                    type="text"
-                                    className="form-control"
-                                    onChange={formik.handleChange}
-                                    value={formik.values.name}
-                                    required
-                                />
-                            </div>
-
-                            <div className="room_inputs">
-                                <label htmlFor="capacity">Capacity</label>
-                                <input
-                                    placeholder="150"
-                                    name="capacity"
-                                    type="text"
-                                    className="form-control"
-                                    value={formik.values.capacity}
-                                    onChange={formik.handleChange}
-                                    required
-                                />
-                            </div>
-
-                            <div className="room_inputs">
-                                <label htmlFor="building">Building</label>
-                                <select className="form-control" onChange={formik.handleChange} name="building" value={formik.values.building} required>
-                                    <option value="">None</option>
-                                    <option value="new_building">New Building</option>
-                                    <option value="main_building">Main Building</option>
-                                    <option value="auditorium">Auditorium</option>
-                                </select>
-                            </div>
-
-                            <div className="room_inputs">
-                                <label htmlFor="type">Type</label>
-                                <select className="form-control" onChange={formik.handleChange} name="type" value={formik.values.type} required>
-                                    <option value="">None</option>
-                                    <option value="lab">Laboratory</option>
-                                    <option value="lecture">Lecture</option>
-                                   
-                                </select>
-                            </div>
-                           
-
-                            <div className="room_buttons">
-                                <button type="button" className="btn">
-                                    Clear
-                                </button>
-                                <button type="submit" className="btn" disabled={!formik.values.name}>
-                                    Add
-                                </button>
-    
-                            </div>
-                        </form>
-                    </div>
-                </div>
+  return (
+    <div className="room">
+      <ScreenNav rightNavData={navData} />
+      <div className="room__container">
+        <div className="room__box">
+          <div className="lead text-success room__message">
+            {loading && clicked && <Spinner Loader={DotLoader} size={30} />}
+            <p className={`lead ${error ? "text-danger" : "text-success"}`}>
+              {!loading && !error && success}
+              {!loading && error && error}
+            </p>
+          </div>
+          <h2 className="text-center text-dark">Add Room Details</h2>
+          <form id="myForm" onSubmit={formik.handleSubmit} autoComplete="off">
+            <div className="room_inputs">
+              <label htmlFor="name">Room Name</label>
+              <input
+                placeholder="A640"
+                name="name"
+                type="text"
+                className="form-control"
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                required
+              />
             </div>
-        );
-};
 
+            <div className="room_inputs">
+              <label htmlFor="capacity">Capacity</label>
+              <input
+                placeholder="150"
+                name="capacity"
+                type="text"
+                className="form-control"
+                value={formik.values.capacity}
+                onChange={formik.handleChange}
+                required
+              />
+            </div>
+
+            <div className="room_inputs">
+              <label htmlFor="building">Building</label>
+              <select
+                className="form-control"
+                onChange={formik.handleChange}
+                name="building"
+                value={formik.values.building}
+                required
+              >
+                <option value="">None</option>
+                <option value="new_building">New Building</option>
+                <option value="main_building">Main Building</option>
+                <option value="auditorium">Auditorium</option>
+              </select>
+            </div>
+
+            <div className="room_inputs">
+              <label htmlFor="type">Type</label>
+              <select
+                className="form-control"
+                onChange={formik.handleChange}
+                name="type"
+                value={formik.values.type}
+                required
+              >
+                <option value="">None</option>
+                <option value="lab">Laboratory</option>
+                <option value="lecture">Lecture</option>
+              </select>
+            </div>
+
+            <div className="room_buttons">
+              <button type="button" className="btn">
+                Clear
+              </button>
+              <button
+                type="submit"
+                className="btn"
+                disabled={!formik.values.name}
+              >
+                Add
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Room;
