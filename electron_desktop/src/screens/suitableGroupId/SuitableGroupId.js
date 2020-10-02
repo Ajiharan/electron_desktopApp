4f271d858e02a85f10113ea297c9a28022af5e0a
Ajiharan <53114753+Ajiharan@users.noreply.github.com>
2020-10-02 17:15:50 +0530

    update issue

--- electron_desktop/src/screens/suitableGroupId/SuitableGroupId.js
+++ electron_desktop/src/screens/suitableGroupId/SuitableGroupId.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addSuitableGroupId } from "../../redux/suitableGroupId/SuitableGroupIdAction";
 import { view_genGroupId } from "../../redux/genId/genIdAction";
+import { view_genSubGroupId } from "../../redux/gensubId/genSubIdAction";
 import { viewRoom } from "../../redux/Room/RoomAction";
 import "./SuitableGroupId.css";
 import { Spinner } from "../animations/Spinner";
@@ -13,27 +14,33 @@ import Select from "react-select";
 const SuitableGroupId = () => {
   const history = useHistory();
   const dispatch = useDispatch();
-  const [groupid_data, setGroupIds] = useState("");
+  const [groupid_data, setGroupIds] = useState([]);
+  const [subgroupid, setSubgroupId] = useState([]);
   const [room_data, setRooms] = useState("");
-  const [selectGroupId, setSelectGroupId] = useState(null);
+  const [selectGroupId, setSelectGroupId] = useState([]);
+  const [selectSubGroupId, setSelectSubGroupId] = useState([]);
   const [selectRoom, setSelectRoom] = useState([]);
   const [clicked, isClicked] = useState(false);
   const [success, setSuccess] = useState("Successfully Added");
+  const [type, setType] = useState("");
   const { loading, error, suitableGroupId } = useSelector(
     (state) => state.addSuitableGroupId
   );
   const { room } = useSelector((state) => state.get_room);
   const { gen_groupids } = useSelector((state) => state.get_genGroupId);
+  const { gen_subgroupids } = useSelector((state) => state.get_genSubGroupId);
 
   useEffect(() => {
     dispatch(viewRoom());
     dispatch(view_genGroupId());
+    dispatch(view_genSubGroupId());
   }, []);
 
   useEffect(() => {
     setRooms(room);
     setGroupIds(gen_groupids);
-  }, [room, gen_groupids]);
+    setSubgroupId(gen_subgroupids);
+  }, [room, gen_groupids, gen_subgroupids]);
 
   const room_options = room.map((data) => {
     console.log(data);
@@ -48,6 +55,14 @@ const SuitableGroupId = () => {
     return {
       value: data.gen_groupid,
       label: data.gen_groupid,
+    };
+  });
+
+  const sub_groupid_options = gen_subgroupids.map((data) => {
+    console.log(data);
+    return {
+      value: data.gen_subgroupid,
+      label: data.gen_subgroupid,
     };
   });
 
@@ -81,6 +96,14 @@ const SuitableGroupId = () => {
       })
     );
   };
+
+  const setCurrentType = () => {
+    if (type === "groupId") {
+      return groupid_options;
+    } else if (type === "subGroupId") {
+      return sub_groupid_options;
+    }
+  };
   return (
     <div className="SuitableGroupId">
       <ScreenNav rightNavData={navData} />
@@ -98,10 +121,24 @@ const SuitableGroupId = () => {
           <form id="frm" onSubmit={(e) => submitHandler(e)}>
             <div className="SuitableGroupId_inputs">
               <label htmlFor="SuitableGroupId" className="text">
+                Category Type
+              </label>
+              <select
+                className="form-control"
+                value={type}
+                onChange={(e) => setType(e.target.value)}
+              >
+                <option value="">select</option>
+                <option value="groupId">groupId</option>
+                <option value="subGroupId">subGroupId</option>
+              </select>
+            </div>
+            <div className="SuitableGroupId_inputs">
+              <label htmlFor="SuitableGroupId" className="text">
                 groupid
               </label>
               <Select
-                options={groupid_options}
+                options={setCurrentType()}
                 className="form-control"
                 classNamePrefix="select"
                 value={selectGroupId}
